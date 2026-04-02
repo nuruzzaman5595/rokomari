3554ab5de2f1bbe0a5b226618bd9712b1d4c8afc
Nuruzzaman Nayon <nayonhossain5595@gmail.com>
2026-04-02 23:15:02 +0600

    webside

---
+++ src/app/About/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div className='text-white text-2xl text-center py-3'>About page here</div>
+  )
+}
+
+export default AboutPage
---
+++ src/app/Contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div className='text-white text-3xl text-center py-3.5'>Contact page here</div>
+  )
+}
+
+export default ContactPage
