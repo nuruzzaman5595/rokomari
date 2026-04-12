ab8544dd4c8d2a05f79e8c6234f6da6aad24be76
Nuruzzaman Nayon <nayonhossain5595@gmail.com>
2026-04-12 22:00:33 +0600

    new change

---
+++ src/app/About/figma/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FigmaPage = () => {
+  return (
+    <div className='text-white text-3xl text-center py-2 '>Figma page here</div>
+  )
+}
+
+export default FigmaPage
--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='text-white text-xl text-center py-2'>Navbar page here</div>
+    <div className='text-white text-xl text-center py-2 container place-items-center'>Navbar page here</div>
   )
 }
 
